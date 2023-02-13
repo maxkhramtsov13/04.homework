adf95f31b8f35d5efe21cdeea2adee289c05c0a8
MaxKhramtsov <maxkhramtsov13@gmail.com>
2023-02-13 16:29:48 +0300

    homework04

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая на вход принимает число
+// и выдаёт, является ли число чётным.
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.Write($"Enter the number -> ");
+		int a = Convert.ToInt32(Console.ReadLine());
+
+		if (a % 2 == 0)
+		{
+			Console.Write($"Even number -> ");
+			Console.WriteLine(a);
+		}
+
+		else
+		{
+			Console.Write($"Odd number -> ");
+			Console.WriteLine(a);
+		}
+	}
+}
